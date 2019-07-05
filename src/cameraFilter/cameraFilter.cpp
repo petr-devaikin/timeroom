cfcb34651018f62a3ce38686dece0e838409fce0
Petr Devaikin <p.devaikin@gmail.com>
2019-07-05 15:42:16 +0200

    camera filter started

---
+++ src/cameraFilter/cameraFilter.cpp
@@ -0,0 +1,12 @@
+//
+//  cameraFilter.cpp
+//  timeroom
+//
+//  Created by Petr Devaikin on 05.07.19.
+//
+
+#include "cameraFilter.hpp"
+
+cameraFilter::cameraFilter(ofxRealSense2 * realSense) {
+    this->realSense = realSense;
+}
---
+++ src/cameraFilter/cameraFilter.hpp
@@ -0,0 +1,22 @@
+//
+//  cameraFilter.hpp
+//  timeroom
+//
+//  Created by Petr Devaikin on 05.07.19.
+//
+
+#ifndef cameraFilter_hpp
+#define cameraFilter_hpp
+
+#include <ofxRealSense2.hpp>
+
+class cameraFilter {
+private:
+    ofxRealSense2 * realSense;
+public:
+    cameraFilter(ofxRealSense2 * realSense);
+    virtual void update() = 0;
+    virtual void draw() = 0;
+};
+
+#endif /* cameraFilter_hpp */
